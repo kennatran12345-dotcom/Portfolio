935d5a7e0813555a779f83eb76ec64f5047c7f41
kennatran12345-dotcom <kennatran12345@gmail.com>
2026-09-18 17:38:02 -0400

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab01")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Web Development - Section D")
+st.subheader("Kenna Tran")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Portfolio**: Learn more about me
+#       2. **Quiz**: Take a fun quiz
+
+st.write("""
+Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1. **Portfolio**: Learn more about me
+2. **Quiz**: Take a fun quiz
+
+
+""")
+
